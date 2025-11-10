30c953d87108722e5d8d753dd193287c90b234c5
wael noufal <noufal@blinkapp.net>
2025-11-11 01:25:34 +0200

    add Contract.b1f and contract.b1f.cs

---
+++ Forms/Contract.b1f.cs
@@ -0,0 +1,30 @@
+﻿using SAPbouiCOM.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ContractManagementAddon.Forms
+{
+    [FormAttribute("ContractManagementAddon.Forms.Contract", "Forms/Contract.b1f")]
+    class Contract : UserFormBase
+    {
+        public Contract()
+        {
+        }
+
+        /// <summary>
+        /// Initialize components. Called by framework after form created.
+        /// </summary>
+        public override void OnInitializeComponent()
+        {
+        }
+
+        /// <summary>
+        /// Initialize form event. Called by framework before form creation.
+        /// </summary>
+        public override void OnInitializeFormEvents()
+        {
+        }
+    }
+}
